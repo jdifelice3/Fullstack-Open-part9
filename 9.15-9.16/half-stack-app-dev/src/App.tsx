cd39b2733f5b33bae2cac1b8a1bc55d76ab8019c
Your Name <johndifelice@protonmail.com>
2025-08-04 16:27:25 -0400

    More type narrowing

--- 9.15-9.16/half-stack-app-dev/src/App.tsx
+++ 9.15-9.16/half-stack-app-dev/src/App.tsx
@@ -1,20 +1,51 @@
-import { HeaderProps, ContentProps } from "./types";
+import { 
+  HeaderProps,
+  ContentProps,
+  CoursePart
+} from "./types";
 
 const App = () => {
   const courseName: string = "Half Stack application development";
-  const courseParts = [
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
-    }
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "requirements",  
+  }
   ];
 
   return (
@@ -24,25 +55,64 @@ const App = () => {
       <Total courseParts={courseParts}/>
     </div>
   );
+
 };
 
 const Header = ( props: HeaderProps ) => {
-  console.log(props);
   return <h1>{props.courseName}</h1>
 }
 
-const Contents = ( props: ContentProps ) => {
+const Contents = (props: ContentProps) => {
   return (
     <div>
-      <p>{props.courseParts[0].name} {props.courseParts[0].exerciseCount} </p>
-      <p>{props.courseParts[1].name} {props.courseParts[1].exerciseCount} </p>
-      <p>{props.courseParts[2].name} {props.courseParts[2].exerciseCount} </p>
-    </div>
-)}
+    {props.courseParts.map(p => {
+      switch (p.kind) {
+        case "basic":
+          return (
+            <div key={p.name}>
+              <b>{p.name} {p.exerciseCount}</b>
+              <p><i>{p.description}</i></p>
+            </div>
+          );
+        case "background":
+          return (
+            <div key={p.name}>
+              <b>{p.name} {p.exerciseCount}</b>
+              <p><i>{p.description}</i></p>
+              <p>Materials: {p.backgroundMaterial}</p>
+            </div>
+          );
+        case "group":
+          return (
+            <div key={p.name}>
+              <b>{p.name} {p.exerciseCount}</b>
+              <p>Projects: {p.groupProjectCount}</p>
+            </div>
+          );
+        case "requirements":
+          return (
+            <div key={p.name}>
+              <b>{p.name} {p.exerciseCount}</b>
+              <p><i>{p.description}</i></p>
+              <p>Requirements: {p.requirements.join(', ')}</p>
+            </div>
+          );
+        default:
+          return assertNever(p); // forces exhaustiveness
+      }
+    })}
+  </div>
+  );  
+};
 
 const Total = ( props: ContentProps ) => {
   const total = props.courseParts.reduce((sum, part) => sum + part.exerciseCount, 0)
   return <p>Number of exercises {total}</p>
 }
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
 export default App;
--- 9.15-9.16/half-stack-app-dev/src/types.ts
+++ 9.15-9.16/half-stack-app-dev/src/types.ts
@@ -2,11 +2,6 @@ export interface HeaderProps {
   courseName: string;
 }
 
-export interface CoursePart {
-    name: string;
-    exerciseCount: number;
-}
-
 export interface ContentProps {
     courseParts: CoursePart[]
 }
@@ -14,3 +9,33 @@ export interface ContentProps {
 export interface TotalProps {
     totalExercises: number;
 }
+
+export interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+export interface CoursePartBasic extends CoursePartBase, CoursePartDescription {
+  kind: "basic"
+}
+
+export interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+export interface CoursePartBackground extends CoursePartBase, CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+export interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+export interface CoursePartRequirements extends CoursePartDescription {
+    requirements: string[];
+    kind: "requirements";
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartRequirements;
